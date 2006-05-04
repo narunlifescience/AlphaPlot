4cc88e05d97b5d61306324ff1beaeb63225e3efd
thzs <thzs@ee5bfcf9-8b04-45a4-a66c-bec449605a02>
2006-05-04 23:19:55 +0000

    - added Doxyfile - added html code for doxygen main page to main.cpp - added doxygen comments to plotWizard.h

--- qtiplot/src/main.cpp
+++ qtiplot/src/main.cpp
@@ -2,6 +2,21 @@
 #include <qaction.h>
 #include "application.h"
 
+// The following stuff is for the doxygen title page
+/*!  \mainpage QtiPlot - Data analysis and scientific plotting - API documentation
+
+  \section description Program description:
+  As a scientist I often need to use data analysis and plotting software. On Windows systems there is a well known and widely used software called <a href="http://www.originlab.com">Origin</a>, which is not free, of course.  My purpose was to develop a free (open source), platform independent clone of Origin. The result is still far away from it's model, but there's a <a class="el" href="http://soft.proindependent.com/wishtolist.html">"wish to"</a> list which I'm constantly working on. All suggestions and contributions are most welcome!</a><br><br>
+  
+  \section libs QtiPlot uses the following libraries:
+  <a href="http://www.trolltech.com/products/qt/index.html">Qt</a>, <a href="http://qwt.sourceforge.net/index.html">Qwt (4.2.0)</a>,  <a href="http://qwtplot3d.sourceforge.net/">QwtPlot3D</a>,
+  <a href="http://sources.redhat.com/gsl/">GSL</a>, 
+  <a href="http://muparser.sourceforge.net/">muParser</a>,
+  <a href="http://www.zlib.net/">zlib (1.2.3)</a>,
+  and <a href="http://sourceforge.net/projects/liborigin/">liborigin</a>.
+  <br>
+*/
+
 int main( int argc, char ** argv ) 
 	{
     QApplication a( argc, argv );
--- qtiplot/src/plotWizard.h
+++ qtiplot/src/plotWizard.h
@@ -1,3 +1,33 @@
+/***************************************************************************
+    File                 : plotWizard.h
+    Project              : QtiPlot
+    --------------------------------------------------------------------
+    Copyright            : (C) 2006 by Ion Vasilief
+    Email                : ion_vasilief@yahoo.fr
+    Description          : A wizard type dialog to create new plots
+                           
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *  This program is free software; you can redistribute it and/or modify   *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation; either version 2 of the License, or      *
+ *  (at your option) any later version.                                    *
+ *                                                                         *
+ *  This program is distributed in the hope that it will be useful,        *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the Free Software           *
+ *   Foundation, Inc., 51 Franklin Street, Fifth Floor,                    *
+ *   Boston, MA  02110-1301  USA                                           *
+ *                                                                         *
+ ***************************************************************************/
+
+
 #ifndef PLOTWIZARD_H
 #define PLOTWIZARD_H
 
@@ -9,42 +39,93 @@ class QPushButton;
 class QListBox;
 class QComboBox;
 
+//! A wizard type dialog class to create new plots
 class plotWizard : public QDialog
 {
     Q_OBJECT
 
 public:
+	//! Constructor
+	/**
+	 * \param parent parent widget
+	 * \param name widget name
+	 * \param modal flag: decides wheather the dialog is model or not
+	 * \param fl Qt window flags
+	 */
     plotWizard( QWidget* parent = 0, const char* name = 0, bool modal = FALSE, WFlags fl = 0 );
+	//! Destructor
     ~plotWizard();
 
-    QPushButton* buttonOk,*buttonXErr, *buttonYErr, *buttonDelete;
-	QPushButton* buttonCancel, *buttonX, *buttonY, *buttonNew, *buttonZ;
-    QButtonGroup* GroupBox1, *GroupBox2, *GroupBox3;
+				//! Button "Plot"
+    QPushButton* buttonOk,
+				//! Button "<->xErr" 
+				*buttonXErr, 
+				//! Button "<->yErr" 
+				*buttonYErr, 
+				//! Button "Delete curve" 
+				*buttonDelete;
+				//! Button "cancel" 
+	QPushButton* buttonCancel,
+				//! Button "<->X" 
+				*buttonX,
+				//! Button "<->Y" 
+				*buttonY,
+				//! Button "New curve" 
+				*buttonNew,
+				//! Button "<->Z" 
+				*buttonZ;
+				//! Button group defining the layout
+    QButtonGroup* GroupBox1, 
+				//! Button group defining the layout
+				*GroupBox2, 
+				//! Button group defining the layout
+				*GroupBox3;
+				//! Combo box to select the table
     QComboBox* boxTables;
-	QListBox *columnsList, *plotAssociations;
+				//! List of the columns in the selected table
+	QListBox *columnsList, 
+				//! List of the plots to generate
+			 *plotAssociations;
 
 public slots:
+	//! Slot: Update the columns list box to contain the columns of 'table'
 	void changeColumnsList(const QString &table);
+	//! Slot: Insert a list of tables into the tables combo box
 	void insertTablesList(const QStringList& tables);
+	//! Slot: Set the contents of the columns list box to the strings in 'cols' 
 	void insertColumnsList(const QStringList& cols);
+	//! Slot: Set the internal columns list to 'cols'
 	void setColumnsList(const QStringList& cols);
+	//! Slot: Add new curve
 	void addCurve();
+	//! Slot: Delete selected curve
 	void toggleCurve();
+	//! Slot: Add column as X
 	void addXCol();
+	//! Slot: Add column as Y
 	void addYCol();
+	//! Slot: Add column as X error
 	void addXErrCol();
+	//! Slot: Add column as Y error
 	void addYErrCol();
+	//! Slot: Accept settings, close the dialog
 	void accept();
+	//! Slot: Display a warning that a new curve must be added first before the selection of the columns
 	bool noCurves();
+	//! Slot: Add column as Z
 	void addZCol();
 
 signals:
+	//! Signal: New plots need to be created
 	void plot (const QStringList&);
+	//! Signal: A new 3D plot needs to be generated
 	void plot3D (const QString&);
+	//! Signal: A new 3D ribbon plot needs to be generated
 	void plot3DRibbon (const QString&);
 
 private:
+	//! Internal list of columns (contains all columns in all tables)
 	QStringList columns;
 };
 
-#endif // EXPORTDIALOG_H
+#endif //  PLOTWIZARD_H
