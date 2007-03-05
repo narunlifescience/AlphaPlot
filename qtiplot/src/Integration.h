f473049b485a4a41ae9b956bcf02b8bda2af4edc
thzs <thzs@ee5bfcf9-8b04-45a4-a66c-bec449605a02>
2007-03-05 23:41:09 +0000

    - another intermediate step

---
+++ qtiplot/src/Integration.h
@@ -0,0 +1,69 @@
+/***************************************************************************
+    File                 : Integration.h
+    Project              : QtiPlot
+    --------------------------------------------------------------------
+    Copyright            : (C) 2007 by Ion Vasilief
+    Email (use @ for *)  : ion_vasilief*yahoo.fr
+    Description          : Numerical integration/differetiation of data sets
+
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *  This program is free software; you can redistribute it and/or modify   *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation; either version 2 of the License, or      *
+ *  (at your option) any later version.                                    *
+ *                                                                         *
+ *  This program is distributed in the hope that it will be useful,        *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the Free Software           *
+ *   Foundation, Inc., 51 Franklin Street, Fifth Floor,                    *
+ *   Boston, MA  02110-1301  USA                                           *
+ *                                                                         *
+ ***************************************************************************/
+#ifndef INTEGRATION_H
+#define INTEGRATION_H
+
+#include "Filter.h"
+
+class Integration : public Filter
+{
+Q_OBJECT
+
+public:
+	Integration(ApplicationWindow *parent, Graph *g);
+	Integration(ApplicationWindow *parent, Graph *g, const QString& curveTitle);
+	Integration(ApplicationWindow *parent, Graph *g, const QString& curveTitle, double start, double end);
+
+    int method(){return d_method;};
+    void setMethod(int n);
+
+private:
+    void init();
+    //!Uses code originally written by Vasileios Gkanis. It needs some more checking.
+    QString logInfo();
+    void output(){};
+
+    //! the integration method: 1 = trapezoidal, max = 5!
+    int d_method;
+};
+
+class Differentiation : public Filter
+{
+Q_OBJECT
+
+public:
+	Differentiation(ApplicationWindow *parent, Graph *g);
+	Differentiation(ApplicationWindow *parent, Graph *g, const QString& curveTitle);
+	Differentiation(ApplicationWindow *parent, Graph *g, const QString& curveTitle, double start, double end);
+
+private:
+    void init();
+    void output();
+};
+#endif
