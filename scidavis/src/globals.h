967ed3f4c71f533da079999d6a10b49e72bf54e1
thzs <thzs@ee5bfcf9-8b04-45a4-a66c-bec449605a02>
2007-08-04 09:55:33 +0000

    - adjusted version number and release date - wrote change log for 0.1.0

--- scidavis/src/globals.h
+++ scidavis/src/globals.h
@@ -37,8 +37,8 @@
  */
 int scidavis_version = 0x000100;
 //! Extra version information string (like "-alpha", "-beta", "-rc1", etc...)
-const char * extra_version = "-beta1";
+const char * extra_version = "";
 //! Copyright string containing the author names etc.
 const char * copyright_string = "Developers (alphabetical order):\nKnut Franke\nTilman Hoener zu Siederdissen\n\nDocumentation:\nRoger Gadiou\n\nSpecial thanks to (alphabetical order):\nQuentin Denis\nGudjon I. Gudjonsson\nAlex Kargovsky\nIon Vasilief\n\nThanks to (no particular order):\nthe developers of Qt, Qwt, QwtPlot3D, GSL, muParser, zlib, Python, PyQt, and liborigin\nall bug reporters, translators and other contributors";
 //! Release date as a string
-const char * release_date = " 2007/00/00";
+const char * release_date = " 2007/08/05";
