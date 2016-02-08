6bbbff5ce8507afdec32278519a0dd7eef5497c9
narunlifescience <n.arun.lifescience@gmail.com>
2016-02-08 04:24:08 -0600

    add splash screen

--- scidavis/src/main.cpp
+++ scidavis/src/main.cpp
@@ -1,6 +1,8 @@
 /* This file is part of AlphaPlot.
+
    Copyright 2006 by Ion Vasilief, Tilman Benkert
    Copyright 2016, Arun Narayanankutty <n.arun.lifescience@gmail.com>
+   
    AlphaPlot is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation, either version 3 of the License, or
@@ -83,11 +85,19 @@ int main( int argc, char ** argv ) {
     ApplicationWindow::about();
     exit(0);
   } else {
+
     ApplicationWindow *mw = new ApplicationWindow();
+
+    // Show splashscreen
+    QPixmap pixmap (":splash/splash.png");
+    QSplashScreen *splash = new QSplashScreen(pixmap);
+    splash->show();
+    
+    // Process more events here before starting app.
     mw->applyUserSettings();
     mw->newTable();
-    mw->activateWindow();
     mw->savedProject();
+
 #ifdef SEARCH_FOR_UPDATES
     if (mw->autoSearchUpdates){
       mw->autoSearchUpdatesRequest = true;
@@ -95,6 +105,10 @@ int main( int argc, char ** argv ) {
     }
 #endif
     mw->parseCommandLineArguments(args);
+
+    // Close splashscreen after 1 sec & show mainwindow
+    QTimer::singleShot(3000, splash, SLOT(close()));
+    QTimer::singleShot(3000, mw, SLOT(activateWindow()));
   }
   app.connect( &app, SIGNAL(lastWindowClosed()), &app,
       SLOT(quit()) );
