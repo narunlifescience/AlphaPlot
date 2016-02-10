ed56cf82bc887cc65a98e2f820b66a240115ea83
narunlifescience <n.arun.lifescience@gmail.com>
2016-02-10 08:31:25 -0600

    add tread outline for multi threading & utilities

---
+++ libscidavis/src/core/Thread.cpp
@@ -0,0 +1,24 @@
+/* This file is part of AlphaPlot.
+
+   Copyright 2016, Arun Narayanankutty <n.arun.lifescience@gmail.com>
+   
+   AlphaPlot is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+   AlphaPlot is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   You should have received a copy of the GNU General Public License
+   along with AlphaPlot.  If not, see <http://www.gnu.org/licenses/>.
+   
+   Description : AlphaPlot thread handler (handles multi threading)
+*/
+
+#include "Thread.h"
+
+void Thread::run() {
+  // probably set the thread priority here
+  QThread::run();
+}
