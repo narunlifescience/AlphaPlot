1827ec48a8f9d03b7c9ebeb0b0c0f0f16dd29f01
narunlifescience <n.arun.lifescience@gmail.com>
2016-03-06 10:39:39 -0600

    add a custom listView widget for new settings dialog

---
+++ scidavis/src/widgets/aSettingsListView.cpp
@@ -0,0 +1,69 @@
+/* This file is part of AlphaPlot.
+   Copyright 2016, Arun Narayanankutty <n.arun.lifescience@gmail.com>
+   
+   AlphaPlot is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+   AlphaPlot is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   You should have received a copy of the GNU General Public License
+   along with AlphaPlot.  If not, see <http://www.gnu.org/licenses/>.
+   
+   Description : widget for settings dialog icon list view catagories.
+*/
+
+#include "aSettingsListView.h"
+
+#include <QPainter>
+
+aSettingsListView::aSettingsListView(QWidget *parent) : QListView(parent) {
+  // Set listview color to window base color & set alpha to 0
+  QString listview_color = "QListView{background-color : rgba(%1,%2,%3,%4);}";
+  setStyleSheet(listview_color.arg(palette().color(QPalette::Base).red())
+                    .arg(palette().color(QPalette::Base).green())
+                    .arg(palette().color(QPalette::Base).blue())
+                    .arg(0));
+
+  // General settings for grid icon view
+  setViewMode(QListView::IconMode);
+  setIconSize(QSize(32, 32));
+  setWordWrap(true);
+  setGridSize(QSize(100, 100));
+  setResizeMode(QListView::Adjust);
+  setDragEnabled(false);
+  setEditTriggers(QAbstractItemView::NoEditTriggers);
+  setSelectionMode(QAbstractItemView::SingleSelection);
+  setMouseTracking(true);
+  setFrameShape(QFrame::NoFrame);
+  viewport()->setAttribute(Qt::WA_Hover);
+  setMovement(QListView::Static);
+  setFocusPolicy(Qt::NoFocus);
+
+  // Set listview hight appropriately
+  setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Fixed);
+}
+
+aSettingsListView::~aSettingsListView() {}
+
+bool aSettingsListView::autoAdjustHeight() {
+  if (!isVisible()) {
+    return false;
+  }
+
+  if (model()->rowCount() > 0) {
+    const int firstItemRectBottom = visualRect(model()->index(0, 0)).bottom();
+    const int lastItemRectBottom =
+        visualRect(model()->index(model()->rowCount() - 1, 0)).bottom();
+    const int gridHeight = gridSize().height();
+    const int final =
+        (((lastItemRectBottom - firstItemRectBottom) / gridHeight) + 1) *
+        gridHeight;
+    setFixedHeight(final + listViewHeightPadding_);
+  } else {
+    setFixedHeight(0);
+  }
+  return true;
+}
---
+++ scidavis/src/widgets/aSettingsListView.h
@@ -0,0 +1,35 @@
+/* This file is part of AlphaPlot.
+   Copyright 2016, Arun Narayanankutty <n.arun.lifescience@gmail.com>
+   
+   AlphaPlot is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+   AlphaPlot is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   You should have received a copy of the GNU General Public License
+   along with AlphaPlot.  If not, see <http://www.gnu.org/licenses/>.
+   
+   Description : widget for settings dialog icon list view catagories.
+*/
+
+#ifndef ASETTINGSLISTVIEW_H
+#define ASETTINGSLISTVIEW_H
+
+#include <QListView>
+
+class aSettingsListView : public QListView {
+  Q_OBJECT
+
+ public:
+  aSettingsListView(QWidget *parent = 0);
+  ~aSettingsListView();
+
+  bool autoAdjustHeight();
+  static const int listViewWidthPadding_ = 10;
+  static const int listViewHeightPadding_ = 5;
+};
+
+#endif  // ASETTINGSLISTVIEW_H
